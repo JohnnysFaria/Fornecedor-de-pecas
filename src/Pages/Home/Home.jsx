bda9f3cbc3fffad1794cec618bec7889bb53244e
= <johnnys.faria@gmail.com>
2023-01-20 17:25:35 -0300

    Firebase ajustes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,32 +1,15 @@
 import React from 'react'
+import Card from '../../Components/Card/Card'
 import Firebase from '../../Components/Firebase/Firebase'
 // import Firebase from '../../Components/Firebase/Firebase'
 import Header from '../../Components/Header/Header'
 import Modal from '../../Components/Modal/Modal'
 import style from './Home.module.css'
 
-const baseUrl = 'https://pokeapi.co/api/v2/';
-
-const api = {
-  getPokemonList: async (offset, limit = 20) => {
-    const response = await fetch(
-      `${baseUrl}pokemon?limit=${limit}&offset=${offset}`,
-    );
-    const json = await response.json();
-    return json;
-  },
-  getPokemonDetails: async (name) => {
-    const response = await fetch(`${baseUrl}pokemon/${name}`);
-    const json = await response.json();
-    console.log(json)
-    return json;
-  },
-};
-
 
 const Home = () => {
   const [modal, setModal] = React.useState(true)
-  const [shop, setShop] = React.useState(null)
+  // const [shop, setShop] = React.useState(null)
 
   
 
@@ -35,8 +18,7 @@ const Home = () => {
     <>
       <Modal modal={modal} setModal={setModal} />
       <Header />
-        {/* <Firebase /> */}
-
+        {/* <Card name={} /> */}
       <Firebase/>
       <div className={style.a}>
       <div className={style.search}>
